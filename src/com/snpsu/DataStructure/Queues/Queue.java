a3c37acd3923236b7f653a50c18ad32bf3b9d43e
jyothi <bhavanaprasad1507@gmail.com>
2026-09-01 16:39:48 +0530

    Stacks,Queues git status

--- src/com/snpsu/DataStructure/Queues/Queue.java
+++ src/com/snpsu/DataStructure/Queues/Queue.java
@@ -1,4 +1,56 @@
 package com.snpsu.DataStructure.Queues;
 
 public class Queue {
+    int[] arr;
+    int front;
+    int back;
+    int size;
+    Queue(int size){
+        this.size=size;
+        arr=new int[size];
+        front=-1;
+        back=-1;
+    }
+    void push(int val){
+        if(back==size-1){
+            System.out.println("Overflown!");
+            return;
+        }
+        back++;
+        arr[back]=val;
+        if(front==-1){
+            front++;
+        }
+    }
+    void pop(){
+        if(front==-1||front>back){
+            System.out.println("Underflown!");
+            return;
+        }
+        arr[front]=0;
+        front++;
+    }
+    int peek(){
+        if(front==-1||front>back){
+            System.out.println("Underflown!");
+            return-1;
+    }
+        return arr[front];
+}
+boolean isEmpty(){
+    return(front==-1||front>back);
+    }
+}
+class Driver{
+    public static void main(String[] args) {
+        Queue qu=new Queue(5);
+        qu.push(10);
+        qu.push(5);
+        qu.push(5);
+        qu.push(7);
+        qu.push(8);
+
+
+
+    }
 }
--- src/com/snpsu/DataStructure/Stack/Stack.java
+++ src/com/snpsu/DataStructure/Stack/Stack.java
@@ -1,4 +1,56 @@
 package com.snpsu.DataStructure.Stack;
 
 public class Stack {
+    int[] arr;
+    int top;
+    int size;
+    Stack(int size){
+        this.size=size;
+        top=-1;
+        arr=new int[size];
+    }
+    void push(int val){
+        if(top==size-1){
+            System.out.println("Overflown!");
+            return;
+        }
+        top++;
+        arr[top]=val;
+    }
+    void pop(){
+        if(top==-1){
+            System.out.println("Stack Underflown!");
+            return;
+        }
+        arr[top]=0;//it clears the memory
+        top--;
+    }
+    int Top(){
+        if(top==-1){
+            System.out.println("Stack Underflown!");
+            return-1;
+        }
+        return arr[top];
+    }
+    boolean isEmpty(){//it returns only true or false
+        return top==-1;
+    }
+}
+class Driver{
+    public static void main(String[] args) {
+        Stack st=new Stack(5);
+        st.push(11);
+        st.push(4);
+        st.push(2);
+        st.push(34);
+        st.push(23);
+        System.out.println(st.Top());
+        st.pop();
+        st.pop();
+        st.pop();
+        st.pop();
+        st.pop();
+        System.out.println(st.Top());
+
+    }
 }
